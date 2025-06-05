f0bea53d01cba1ffbf7df2896b64bf7d66f92233
Abreham Kassa <abrehamkassa19@gmail.com>
2025-06-05 13:36:59 +0300

    feat: Define handler and helper functions to create a task

--- handlers.go
+++ handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"encoding/json"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -10,4 +11,21 @@ import (
 func healthCheck(w http.ResponseWriter, r *http.Request, _  httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	http.ServeFile(w, r, "index.html")
+}
+
+func createTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var t Task
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	taskMux.Lock()
+	t.ID = nextID
+	nextID++
+	tasks[t.ID] = t
+	saveTasks()
+	taskMux.Unlock()
+
+	jsonResponse(w, t, http.StatusCreated)
 }
--- main.go
+++ main.go
@@ -3,14 +3,55 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
+	"os"
+	"encoding/json"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+
+type Task struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Details string `json:"details"`
+	Done    bool   `json:"done"`
+}
+
+var (
+	tasks   = make(map[int]Task)
+	nextID  = 1
+	taskMux = sync.Mutex{}
+	dataFile = "tasks.json"
+)
+
+func saveTasks() {
+	var taskList []Task
+	for _, t := range tasks {
+		taskList = append(taskList, t)
+	}
+	data, err := json.MarshalIndent(taskList, "", "  ")
+	if err != nil {
+		log.Printf("Failed to encode tasks: %v", err)
+		return
+	}
+	if err := os.WriteFile(dataFile, data, 0644); err != nil {
+		log.Printf("Failed to write data file: %v", err)
+	}
+}
+
+func jsonResponse(w http.ResponseWriter, data any, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
 func main() {
 	// Router
 	router := httprouter.New()
 
 	router.GET("/health_check", healthCheck)
+	router.POST("/api/tasks", createTask)
 
 	// Server
 	server := &http.Server {
